c9c40757cf8215fbcc62180b612b7de621ea637b
JSON <110289335+Isreal-Oparanti@users.noreply.github.com>
2024-10-05 18:30:12 +0100

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const { register, login} = require('../controllers/AuthController.js');
-const { createJob, getAllJobs } = require('../controllers/job.js');
- 
+const { register, login, updateUser, getUser, getProfile, acceptApplication, cancelApplication ,updateOrder,saveReview } = require('../controllers/AuthController.js');
+const { createJob, getAllJobs, getAJob, applyForJob, getApplyJobs } = require('../controllers/job.js');
+// const  localVariables  = require('../middleware/auth.js')
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/create-jobs', createJob);
-
+router.put('/update', updateUser)
 router.get('/get-jobs', getAllJobs);
- 
+router.post('/update-order', updateOrder)
+router.post('/get-user', getUser);
+router.post('/get-profile', getProfile)
+router.post('/get-apply-jobs', getApplyJobs);
+router.post('/save-review', saveReview)
+router.post('/get-job', getAJob);
+router.post('/apply', applyForJob)
+router.post('/accept-application', acceptApplication)
+router.post('/cancel-application', cancelApplication)
+
 module.exports = router;
     
