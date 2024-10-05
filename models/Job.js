ef89d30a1e41331cda346038eb27cfeeae476d71
JSON <110289335+Isreal-Oparanti@users.noreply.github.com>
2024-10-05 18:30:41 +0100

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,34 +1,15 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-  jobTitle: {
-    type: String,
-    required: true
-  },
-  tags: {
-    type: [String],
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  token: {
-    type: String,
-    required: true
-  },
-  amount: {
-    type: Number,
-    required: true
-  }
+  jobTitle: { type: String, required: true },
+  tags: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  applied: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // Refers to the User model
 });
 
-const Job = mongoose.model('Job', jobSchema);
-
-module.exports = Job;
-
-// module.exports = mongoose.model("Message", MessageSchema);
+module.exports = mongoose.model('Job', jobSchema);
