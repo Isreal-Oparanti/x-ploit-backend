cd5e9bdfcc68f70792aa5f479aaa4f2c2c7710ce
JSON <110289335+Isreal-Oparanti@users.noreply.github.com>
2024-10-05 18:31:02 +0100

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,38 +1,67 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// models/UserModel.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-   
-    username: {
-        type: String,
-        required: [true, "Please provide a unique Username"],
-        unique: [true, "Username Exist"]
+const UserSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: [true, "Please provide your first name"],
     },
-    address: {type: String},
-     
-    // plan: { type: String },
-    
-    // address: { type: String},
-    // profile: { type: String}
+    lastname: {
+      type: String,
+      required: [true, "Please provide your last name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide a valid email"],
+      unique: [true, "Email already exists"],
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    description: {
+      // Corrected spelling from 'discription' to 'description'
+      type: String,
+    },
+    wallet: {
+      type: String,
+    },
+    skills: [],
+
+    jobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }],
+    orders: [],
+    reviews: [],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-// Mongoose middleware to hash the password before saving
-// UserSchema.pre('save', async function (next) {
-//     if (!this.isModified('password')) {
-//         return next();
-//     }
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
-//     try {
-//         const salt = await bcrypt.genSalt(10);
-//         this.password = await bcrypt.hash(this.password, salt);
-//         return next();
-//     } catch (error) {
-//         return next(error);
-//     }
-// });
-
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = UserModel;
