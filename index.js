f80e0f9f9a4620ae2598223399b800bbc7b51ada
JSON <110289335+Isreal-Oparanti@users.noreply.github.com>
2024-10-05 18:28:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const mongoose = require('mongoose');
-  const dotenv = require('dotenv');
-  const cors = require('cors');
-  const otpGenerator = require('otp-generator')
-   
-  const routes = require('./routes/Routes.js');
- 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const otpGenerator = require("otp-generator");
 
+const routes = require("./routes/Routes.js");
+// const PasswordRecoveryRoute = require('./routes/PasswordRecoveryRoute.js');
 
-  const app = express();
+const app = express();
 
-  // env configurations
-  dotenv.config();
+// env configurations
+dotenv.config();
 
-   
-  const mongoURI = "mongodb+srv://Isreal:oparanti@cluster0.pkhckh1.mongodb.net/hackathon?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI =
+  "mongodb+srv://Isreal:oparanti@cluster0.pkhckh1.mongodb.net/LancePoint?retryWrites=true&w=majority&appName=Cluster0";
+// const mongoURI = "mongodb://localhost:27017/Hackathon"
 
-
-  mongoose.connect(mongoURI, {
+mongoose
+  .connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then(() => {
-      console.log('MongoDB connected');
-    })
-    .catch(err => {
-      console.error('Error connecting to MongoDB:', err);
-    });
-  
-     
-  // Middlewares
-  app.use(bodyParser.json({ limit: '30mb', extended: true }));
-  app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-  app.use(cors());
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
  
-  ///routes
-  app.use('/api', routes)
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
+ 
+app.use("/api", routes);
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
