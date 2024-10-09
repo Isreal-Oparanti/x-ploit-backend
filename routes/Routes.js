d27387a1624a7dde62e9dae4e428e97b2b3aaee4
JSON <110289335+Isreal-Oparanti@users.noreply.github.com>
2024-10-09 14:21:33 +0100

    Update Routes.js

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { register, login, updateUser, getUser, getProfile, acceptApplication, cancelApplication ,updateOrder,saveReview } = require('../controllers/AuthController.js');
+const { register, login, googleLogin, updateUser, getUser, getProfile, acceptApplication, cancelApplication ,updateOrder,saveReview } = require('../controllers/AuthController.js');
 const { createJob, getAllJobs, getAJob, applyForJob, getApplyJobs } = require('../controllers/job.js');
 // const  localVariables  = require('../middleware/auth.js')
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/google-login', googleLogin)
 router.post('/create-jobs', createJob);
 router.put('/update', updateUser)
 router.get('/get-jobs', getAllJobs);
